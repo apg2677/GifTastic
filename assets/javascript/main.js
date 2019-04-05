96a4a4bdcabb8c99b57d41dfc723f9dee4b7c567
Amir P <apourrezaei@gmail.com>
2019-04-05 17:27:22 -0400

    Stable

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,13 +1,13 @@
 var topics = ["Guitar", "Drums", "Bass", "Piano", "Saxaphone", "Trumpet"];
 
 
-$(document).ready(function() {
+$(document).ready(function () {
     Initialize();
     
 });
 
 function RenderButtons() {
-    for(var i= 0;i<topics.length;i++) {
+    for (var i = 0; i < topics.length; i++) {
         var tempButton = $("<button>");
         tempButton.attr("data-name", topics[i]);
         tempButton.attr("class", "topic");
@@ -19,28 +19,43 @@ function RenderButtons() {
 function Initialize() {
     RenderButtons();
     $(".topic").click(GetTopic);
+    $("#btnAdd").click(AddButton);
 }
-
 function GetTopic() {
     var choice = $(this).attr("data-name");
     var requestUrl = "https://api.giphy.com/v1/gifs/search?q=" + choice + "&api_key=L6xowQE91UNaYXFSoXvO5mEkxcG0Y3Tn&limit=5";
     $.ajax({
         url: requestUrl,
-        method:"GET"
-    }).then(function(response) {
+        method: "GET"
+    }).then(function (response) {
         console.log(response);
-        
+
         // $("#gif-view").text(JSON.stringify(response));
         RenderGifs(response);
     });
 }
 
 function RenderGifs(res) {
-    for (var i=0;i<5;i++) {
+    for (var i = 0; i < 10; i++) {
         console.log(res.data[i].images.preview_gif);
         var tempGif = $("<img>");
         tempGif.attr("src", res.data[i].images.preview_gif.url);
-        $("#gif-view").append(tempGif);
+        $("#gif-view").prepend(tempGif);
     }
 
+}
+
+function AddButton() {
+    var newTopic = $("#newTopic").val();
+    topics.push(newTopic);
+    // $("#buttons").empty();
+    console.log(newTopic);
+    console.log(topics);
+    // RenderButtons();
+    var tempButton = $("<button>");
+        tempButton.attr("data-name", newTopic);
+        tempButton.attr("class", "topic");
+        tempButton.text(newTopic);
+        $("#buttons").append(tempButton);
+        $(".topic").click(GetTopic);
 }
