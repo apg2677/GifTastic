2015727d4ea094e684339b36b90f4ff63fa71c48
Amir P <apourrezaei@gmail.com>
2019-04-06 14:02:25 -0400

    Giphy click stable

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,5 +1,5 @@
 var topics = ["Guitar", "Drums", "Bass", "Piano", "Saxaphone", "Trumpet"];
-var still = true;
+var still;
 
 $(document).ready(function () {
     Initialize();
@@ -21,25 +21,11 @@ function Initialize() {
     RenderButtons();
     $(".topic").click(GetTopic);
     $("#btnAdd").click(AddButton);
-    // $(".giphy").click(function() {
-    //     console.log("Clicked image!");
-    // });
+
 }
 function GetTopic() {
     var choice = $(this).attr("data-name");
-    if(still==true) {
-        
-        var state = $(this).attr("data-state");
-        console.log("Still: "+ still);
-        still = !still;
-       
-    }
-    else {
-        
-        var state = $(this).attr("data-state");
-        console.log("Still: " + still);
-        still = !still;
-    }
+    
     var requestUrl = "https://api.giphy.com/v1/gifs/search?q=" + choice + "&api_key=L6xowQE91UNaYXFSoXvO5mEkxcG0Y3Tn&limit=10";
     $.ajax({
         url: requestUrl,
@@ -52,15 +38,39 @@ function GetTopic() {
     });
 }
 
+const GiphyClick = function () {
+    if(still) {
+        
+        var state = $(this).attr("data-state");
+        console.log("Still: "+ still);
+        // Change data source to animate
+        var dataStill = $(this).attr("data-still");
+
+        $(this).attr("src", dataStill);
+        still = !still;
+       
+    }
+    else {
+        
+        var state = $(this).attr("data-state");
+        console.log("Still: " + still);
+        // change data source to still
+        var dataAnimate = $(this).attr("data-animate");
+        $(this).attr("src", dataAnimate);
+        still = !still;
+    }
+};
 function RenderGifs(res) {
     for (var i = 0; i < 10; i++) {
-        console.log(res.data[i].images.preview_gif);
+        // console.log(res.data[i].images.preview_gif);
         var tempGif = $("<img>");
         tempGif.attr("class", "giphy");
-        tempGif.click(function() {
-            alert("Clicked Image!");
-        })
-        tempGif.attr("src", res.data[i].images.fixed_height_still.url);
+        tempGif.attr("data-still", res.data[i].images.fixed_height_still.url);
+        tempGif.attr("data-animate", res.data[i].images.fixed_height.url);
+        tempGif.attr("src", res.data[i].images.fixed_height_still.url );
+        tempGif.click(GiphyClick);
+
+    
         $("#gif-view").prepend(tempGif);
     }
 
@@ -70,8 +80,8 @@ function AddButton() {
     var newTopic = $("#newTopic").val();
     topics.push(newTopic);
     // $("#buttons").empty();
-    console.log(newTopic);
-    console.log(topics);
+    // console.log(newTopic);
+    // console.log(topics);
     // RenderButtons();
     var tempButton = $("<button>");
         tempButton.attr("data-name", newTopic);
