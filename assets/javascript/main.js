19447358d889a1a02ba4c625fb60e9731b39edfd
Amir P <apourrezaei@gmail.com>
2019-04-06 13:39:39 -0400

    Image click

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,15 +1,16 @@
 var topics = ["Guitar", "Drums", "Bass", "Piano", "Saxaphone", "Trumpet"];
-
+var still = true;
 
 $(document).ready(function () {
     Initialize();
-    
+   
 });
 
 function RenderButtons() {
     for (var i = 0; i < topics.length; i++) {
         var tempButton = $("<button>");
         tempButton.attr("data-name", topics[i]);
+        tempButton.attr("data-state", still);
         tempButton.attr("class", "topic");
         tempButton.text(topics[i]);
         $("#buttons").append(tempButton);
@@ -20,10 +21,26 @@ function Initialize() {
     RenderButtons();
     $(".topic").click(GetTopic);
     $("#btnAdd").click(AddButton);
+    // $(".giphy").click(function() {
+    //     console.log("Clicked image!");
+    // });
 }
 function GetTopic() {
     var choice = $(this).attr("data-name");
-    var requestUrl = "https://api.giphy.com/v1/gifs/search?q=" + choice + "&api_key=L6xowQE91UNaYXFSoXvO5mEkxcG0Y3Tn&limit=5";
+    if(still==true) {
+        
+        var state = $(this).attr("data-state");
+        console.log("Still: "+ still);
+        still = !still;
+       
+    }
+    else {
+        
+        var state = $(this).attr("data-state");
+        console.log("Still: " + still);
+        still = !still;
+    }
+    var requestUrl = "https://api.giphy.com/v1/gifs/search?q=" + choice + "&api_key=L6xowQE91UNaYXFSoXvO5mEkxcG0Y3Tn&limit=10";
     $.ajax({
         url: requestUrl,
         method: "GET"
@@ -39,7 +56,11 @@ function RenderGifs(res) {
     for (var i = 0; i < 10; i++) {
         console.log(res.data[i].images.preview_gif);
         var tempGif = $("<img>");
-        tempGif.attr("src", res.data[i].images.preview_gif.url);
+        tempGif.attr("class", "giphy");
+        tempGif.click(function() {
+            alert("Clicked Image!");
+        })
+        tempGif.attr("src", res.data[i].images.fixed_height_still.url);
         $("#gif-view").prepend(tempGif);
     }
 
